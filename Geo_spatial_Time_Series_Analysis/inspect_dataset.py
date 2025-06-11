d92d2f986eda6ce840af8e39b40e8a7e9d2bcc8b
shweta27407 <shwetabambal18@gmail.com>
2025-06-11 23:57:29 +0200

    removing large datasets

---
+++ Geo_spatial_Time_Series_Analysis/inspect_dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Load your dataset
+df = pd.read_csv('/Users/shwetabambal/Documents/myrepos/data-science-/Geo_spatial_Time_Series_Analysis/dataset_40_percent.csv')
+
+# Sample 40% of the data (removing 60%)
+reduced_df = df.sample(frac=0.1, random_state=42)  # random_state ensures reproducibility
+
+# Save the reduced dataset
+reduced_df.to_csv('/Users/shwetabambal/Documents/myrepos/data-science-/Geo_spatial_Time_Series_Analysis/dataset_10_percent.csv', index=False)
